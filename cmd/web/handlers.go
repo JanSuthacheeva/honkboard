5d4a812d0735e394e10d1ca79dfde39e37aa3b36
Jan Suthacheeva <j.steikowski@aol.com>
2025-05-15 20:23:58 +0700

    Adjust landing page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,5 +4,6 @@ import "net/http"
 
 func (app *application) landingPage(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+	data.ShowFooter = false
 	app.render(w, r, http.StatusOK, "landing.html", "base", data)
 }
